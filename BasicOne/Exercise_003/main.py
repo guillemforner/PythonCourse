66d332afaab74e24c3a3bad34b1999d6869b52a1
guillem.forner <guillem.forner@gmail.com>
2020-05-09 12:18:26 +0200

    Initial commit

---
+++ BasicOne/Exercise_003/main.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+print("Current date and time: ")
+now=datetime.now()
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
---
+++ BasicOne/Exercise_004/main.py
@@ -0,0 +1,5 @@
+import math
+
+print("Input de radius of a circle: ", end="")
+r = float(input())
+print("The area of a circle of radius", str(r), "is: ", math.pi*r**2)
